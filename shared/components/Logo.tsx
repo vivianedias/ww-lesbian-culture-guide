5b07be71948e32e9a4e5fb052fc789c5519eec3c
Camila Cardoso <caamila.cardoso.l@gmail.com>
2023-04-09 21:03:33 -0300

    feat: styles logo

--- shared/components/Logo.tsx
+++ shared/components/Logo.tsx
@@ -1,17 +1,22 @@
-import { Box, VStack, HStack, Text, useColorModeValue } from '@chakra-ui/react'
-import MapIcon from './MapIcon'
+import {
+  Box,
+  VStack,
+  HStack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import MapIcon from "./MapIcon";
 
 export default function Logo() {
-  const logoTextColor = useColorModeValue("gray.600", "gray.50")
+  const logoTextColor = useColorModeValue("gray.600", "gray.50");
   return (
     <Box fontFamily={'"Comfortaa", cursive'}>
-      <VStack align={"flex-end"} spacing={0} color={logoTextColor} fontSize={"lg"}>
-        <HStack>
-          <MapIcon boxSize={6} />
-          <Text>lesbian</Text>
+      <VStack spacing={0} color={logoTextColor} fontSize={"lg"}>
+        <HStack align={"flex-end"}>
+          <MapIcon boxSize={10} />
+          <Text>lesbian culture guide</Text>
         </HStack>
-        <Text>culture guide</Text>
       </VStack>
     </Box>
-  )
+  );
 }
